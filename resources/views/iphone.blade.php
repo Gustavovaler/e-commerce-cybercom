8877e1903cd51598486b8ac3510b960c53e5123b
Sebastian Maciel <sebamaciel15@gmail.com>
2020-08-25 17:03:15 -0300

    agregue todas las rutas

--- resources/views/iphone.blade.php
+++ resources/views/iphone.blade.php
@@ -3,7 +3,5 @@
 
 
 @section('content')
-    
-
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/android', function(){
+    return view ('android');
+});
+
+Route::get('/iphone', function(){
+    return view ('iphone');
+});
+
+Route::get('/informatica', function(){
+    return view ('informatica');
+});
+
+Route::get('/audio-video', function(){
+    return view ('audio-video');
+});
+
+Route::get('/soporte', function(){
+    return view ('soporte');
+});
+
+Route::get('/donde-comprar', function(){
+    return view ('donde-comprar');
+});
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
