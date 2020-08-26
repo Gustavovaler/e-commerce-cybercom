40e1f0b29d102024374a4b2dd56d8b52a33e06b6
Sebastian Maciel <sebamaciel15@gmail.com>
2020-08-26 14:55:16 -0300

    acabo de integrar el cdn de vue.js, ahora si podemos utilizar vue para el front-end.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
   crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+
 {{-- ------------ESTO ES LO QUE HICISTE SEBASTIAN ----------------- --}}
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         
