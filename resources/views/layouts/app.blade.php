b4378f3435eca4acbda763adb0042bc908a6a6fa
gustavo <gustavodevaler@gmail.com>
2020-08-28 23:22:40 -0300

    cambios en nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,43 +13,51 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+    <link rel="stylesheet" href="{{asset('css/estilos_nav.css')}}">
 
 {{-- ------------ESTO ES LO QUE HICISTE SEBASTIAN ----------------- --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark nuestra ">
         
       <a class="navbar-brand" href="#"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
-    
+      </button>    
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav m-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/android">Android</a>
+            <a class="nav-link active" href="/android">Android</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/iphone">Iphone</a>
+            <a class="nav-link active" href="/iphone">iPhone</a>
           </li>    
           <li class="nav-item">
-            <a class="nav-link" href="/informatica">Informatica</a>
+            <a class="nav-link active" href="/informatica">Informatica</a>
           </li>  
           <li class="nav-item">
-            <a class="nav-link" href="/audio-video">Audio y video</a>
+            <a class="nav-link active" href="/audio-video">Audio y video</a>
           </li> 
           <li class="nav-item">
-            <a class="nav-link" href="/soporte">Soporte</a>
+            <a class="nav-link active" href="/soporte">Soporte</a>
           </li>   
           <li class="nav-item">
-            <a class="nav-link" href="/donde-comprar">Donde comprar</a>
-          </li>   
+            <a class="nav-link active" href="/donde-comprar">Donde comprar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="/search">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+          </svg>
+            </a>
+          </li>
+          
+          
+          
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" >
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit" placeholder="Buscar">Buscar</button>
-        </form>
+        
       </div>
     </nav>
 
